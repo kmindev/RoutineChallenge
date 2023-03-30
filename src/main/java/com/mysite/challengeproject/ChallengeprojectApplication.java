fbdf1aa11f1ab80ab9eab40ca3b2aeab7c5fd163
ssssudan <kankgm0324@gmail.com>
2023-03-30 16:42:06 +0900

    Initial commit

---
+++ src/main/java/com/mysite/challengeproject/ChallengeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.challengeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeprojectApplication.class, args);
+	}
+
+}
