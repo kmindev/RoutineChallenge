544a5cff97149f0d03dbab140ccd0235fc5c49d7
DESKTOP-OG66SG0\daily <sseco91@gmail.com>
2023-04-01 17:13:16 +0900

    Feat : member 기능 구현

--- src/main/java/com/mysite/challengeproject/member/MemberService.java
+++ src/main/java/com/mysite/challengeproject/member/MemberService.java
@@ -1,5 +1,14 @@
 package com.mysite.challengeproject.member;
 
-public interface MemberService {
+import java.util.ArrayList;
 
+public interface MemberService {
+	public int insertMember(MemberVO member);
+	public int login(MemberVO member);
+	public int findId(MemberVO member);
+	public int findPw(MemberVO member);
+	public int updateMember(MemberVO member);
+	public int getMember(MemberVO member);
+	public ArrayList<MemberVO> getMemberList();	
+	public int deleteMember(MemberVO member);
 }
