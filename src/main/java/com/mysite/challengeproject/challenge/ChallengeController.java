9af6a89105e84fb5b5d83335dfc793214a42917b
ssssudan <kankgm0324@gmail.com>
2023-04-03 00:02:31 +0900

    Fix: "파라메터를 전달받을 때 @RequestBody, @RequestParam 어노테이션 추가"
    Get요청을 받을 때 @RequestParam를 추가
    리액트에서 JSon 형식으로 요청하기 때문에 RequestBody를 추가

--- src/main/java/com/mysite/challengeproject/challenge/ChallengeController.java
+++ src/main/java/com/mysite/challengeproject/challenge/ChallengeController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,13 +26,13 @@ public class ChallengeController {
 		
 	// 주제별 챌린지 조회
 	@GetMapping("/theme_challengelist")
-	public List<ChallengeDTO> ThemeChallengelist(String theme) {
-		return challengeServiceImpl.ThemeChallengelist(theme);
+	public List<ChallengeDTO> ThemeChallengelist(@RequestParam("challenge_theme") String challenge_theme) {
+		return challengeServiceImpl.ThemeChallengelist(challenge_theme);
 	}
 	
 	// 진행상태별 챌린지 조회(0:진행예정 1:진행중 2:진행종료)
 	@GetMapping("/state_challengelist")
-	public List<ChallengeDTO> StateChallengelist(int state) {
+	public List<ChallengeDTO> StateChallengelist(@RequestParam("state") int state) {
 		List<ChallengeDTO> list;
 		if (state == 0) {
 			list = challengeServiceImpl.StateChallengelist0(state);
@@ -45,20 +47,20 @@ public class ChallengeController {
 	// 챌린지 생성
 	// 챌린지 대표사진, 챌린지 사진 서버에 저장하고, 서버 저장 path를 db에 저장하는 부분이 추가되야함(react에서 먼저 작업하고 하는게 좋을거 같음) 
 	@PostMapping("/create_challenge")
-	public int insertChallenge(ChallengeVO challenge) {
+	public int insertChallenge(@RequestBody ChallengeVO challenge) {
 		return challengeServiceImpl.insertChallenge(challenge);
 	}
 
 	// 챌린지 상세정보 조회
 	@GetMapping("/detail_challenge")
-	public ChallengeVO getDetailChallenge(int challenge_num) {
+	public ChallengeVO getDetailChallenge(@RequestParam("challenge_num") int challenge_num) {
 		challengeServiceImpl.setReadCountUpdate(challenge_num);
 		return challengeServiceImpl.getDetailChallenge(challenge_num);
 	}
 
 	// 참여중인 챌린지 조회
 	@GetMapping("/my_challengelist")
-   	public List<ChallengeDTO>getMyChallengeList(String member_id) {
+   	public List<ChallengeDTO>getMyChallengeList(@RequestParam("member_id") String member_id) {
 		return challengeServiceImpl.getMyChallengeList(member_id);
 	}
 }
