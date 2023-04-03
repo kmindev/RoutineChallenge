29734bb86b2c524b8ecf3e1d1f073c668c4e377c
Sejin_ <w_star25@naver.com>
2023-04-04 02:00:00 +0900

    Docs: "마이페이지 스타일 수정, footer 주석 처리"

--- src/Project/Layout/Layout3.js
+++ src/Project/Layout/Layout3.js
@@ -1,7 +1,7 @@
-import Header3 from './Header3';
-import Footer2 from './Footer2';
-import { Outlet } from 'react-router-dom';
-import Sidebar from '../MyPage/Sidebar';
+import Header3 from "./Header3";
+import Footer2 from "./Footer2";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../MyPage/Sidebar";
 
 const Layout3 = () => {
   return (
@@ -9,7 +9,7 @@ const Layout3 = () => {
       <Header3 />
       <Sidebar />
       <Outlet />
-      <Footer2 />
+      {/* <Footer2 /> */}
     </>
   );
 };
