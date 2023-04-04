d2f8573c20365f973af4453878060daa6759fdf6
Sejin_ <w_star25@naver.com>
2023-04-04 21:16:30 +0900

    Docs: "console.log 삭제"

--- src/Project/Challenge/CreateChallenge.js
+++ src/Project/Challenge/CreateChallenge.js
@@ -8,7 +8,6 @@ const CreateChallenge = () => {
   useEffect(() => {
     // 로그인 상태인지 체크
     const login_id = window.sessionStorage.getItem("member_id");
-    console.log("window.sessionStorage(login_id) => ", login_id);
     if (login_id === null) {
       alert("로그인 후 사용 가능합니다!!");
       loginnavigate("/login");
