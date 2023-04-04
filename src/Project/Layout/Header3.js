de3eaeb8027ca1258348fb5cb3f15019c00a9e06
Sejin_ <w_star25@naver.com>
2023-04-04 22:23:48 +0900

    Feat: "로그아웃 시 홈으로 이동"

--- src/Project/Layout/Header3.js
+++ src/Project/Layout/Header3.js
@@ -1,7 +1,7 @@
-import './Header3.css';
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import "./Header3.css";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Header3 = () => {
   const [user, setUser] = useState({});
@@ -22,7 +22,7 @@ const Header3 = () => {
     getUser();
   }, []);
 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const linkToMain = () => {
     navigate("/");
@@ -35,25 +35,42 @@ const Header3 = () => {
       "handleLogout: window.sessionStorage(login_id) =>",
       window.sessionStorage.getItem("member_id")
     );
-    alert('내일 또 만나요!');
+    alert("내일 또 만나요!");
+    navigate("/");
   };
 
-  
   return (
     <div>
       <header>
         <section className="header3">
           <div className="logoText">
-            <p onClick={linkToMain}><a href="/"><img src={process.env.PUBLIC_URL + "/image/logo/logo.png"} alt="logo" /></a></p>          
-            <p><span>{user.member_nickname}</span> 챌린저의 하루를 응원합니다!</p>
+            <p onClick={linkToMain}>
+              <a href="/">
+                <img
+                  src={process.env.PUBLIC_URL + "/image/logo/logo.png"}
+                  alt="logo"
+                />
+              </a>
+            </p>
+            <p>
+              <span>{user.member_nickname}</span> 챌린저의 하루를 응원합니다!
+            </p>
           </div>
-          <div className='rNav'>
-            <ul className='nav'>
-              <li className='l04'><a href="/#about">ABOUT</a></li>
-              <li className='l04'><a href="/challenge">CHALLENGE</a></li>
-              <li className='l04'><a href="#" onClick={handleLogout}>LOGOUT</a></li>
+          <div className="rNav">
+            <ul className="nav">
+              <li className="l04">
+                <a href="/#about">ABOUT</a>
+              </li>
+              <li className="l04">
+                <a href="/challenge">CHALLENGE</a>
+              </li>
+              <li className="l04">
+                <a href="#" onClick={handleLogout}>
+                  LOGOUT
+                </a>
+              </li>
             </ul>
-          </div>        
+          </div>
         </section>
       </header>
     </div>
