80bc7de96763743a66b7514d95abbc7033c2055c
DESKTOP-OG66SG0\daily <sseco91@gmail.com>
2023-04-04 10:29:22 +0900

    Design : join css 수정 / 챌린지생성페이지 대표이미지 부분 디자인 수정 Docs : 챌린지생성페이지 수정 Remove : 챌린지생성페이지 수정 후 불필요한 파일 삭제 화살표 이미지 추가

--- src/Project/Layout/Header2.js
+++ src/Project/Layout/Header2.js
@@ -8,7 +8,7 @@ const Header2 = () => {
         <div className="header">
           <nav className="nav">
             <ul className='nav-left'>
-              <li className='l03'><a href="#about">ABOUT</a></li>
+              <li className='l03'><a href="/#about">ABOUT</a></li>
               <li className='l03'><a href="/challenge">CHALLENGE</a></li>
             </ul>
             <ul className='nav-center'>
